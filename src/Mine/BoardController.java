559737258c99aa1f0b7123ef1d0416d8abf15a9c
CaptainJensen <hunterjensen9@gmail.com>
2017-06-28 23:56:33 -0400

    Added styling to board, Added images for game

--- src/Mine/BoardController.java
+++ src/Mine/BoardController.java
@@ -1,8 +1,39 @@
 package Mine;
 
-public class BoardController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BoardController implements Initializable {
+
+    @FXML
+    private ToggleButton  T0011;
+    private ToggleButton  T0010;
 
 
 
 
+
+
+
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+
+
+
+    }
 }
--- src/Mine/Main.java
+++ src/Mine/Main.java
@@ -11,8 +11,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Board.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Minesweeper");
+        primaryStage.centerOnScreen(); //TODO: Find a way to center on open. Works without alerting to page
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
